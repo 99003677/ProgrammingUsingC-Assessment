e4497486cdb88b83769aa90d1931d81b76a3db3e
Divyam Jalota <divyam.jalota@ltts.com>
2021-02-25 16:22:48 +0530

    2nd problem done

--- ProblemTwo_CompilationError/DirectReport.cs
+++ ProblemTwo_CompilationError/DirectReport.cs
@@ -6,15 +6,20 @@ using System.Threading.Tasks;
 
 namespace ProblemTwo_CompilationError
 {
-    class DirectReport
+    class DirectReport : Employee
     {
         public Employee ReportsTo { get; set; }
         public string ProjectCode { get; set; }
 
-        public DirectReport(Employee managerEmpObj,string pCode) :base(eID,eName,eLevel,eDoJ)
+        public DirectReport(int eId, string eName, int eLevel, DateTime eDoj, Employee managerEmpObj,string pCode) :base(eId, eName,eLevel,eDoj)
         {
             ReportsTo = managerEmpObj;
             ProjectCode = pCode;
+            EmpID = eId;
+            EmpName = eName;
+            EmpLevel = eLevel;
+            DateOfJoining = eDoj;
+
         }
     }
 }
--- ProblemTwo_CompilationError/Manager.cs
+++ ProblemTwo_CompilationError/Manager.cs
@@ -6,15 +6,19 @@ using System.Threading.Tasks;
 
 namespace ProblemTwo_CompilationError
 {
-    class Manager
+    class Manager : Employee
     {
         public List<Employee> DirectReports { get; set; }
         public string CostCode { get; set; }
 
-        public Manager(List<Employee> drEmployeesLst, string cCode) :base(eID, eName, eLevel, eDoJ)
+        public Manager(int eId, string eName, int eLevel, DateTime eDoj, List<Employee> drEmployeesLst, string cCode) :base(eId, eName, eLevel, eDoj)
         {
             DirectReports = drEmployeesLst;
             CostCode = cCode;
+            EmpID = eId;
+            EmpName = eName;
+            EmpLevel = eLevel;
+            DateOfJoining = eDoj;
 
         }
 
